9015d77427a0957ee4f51889381e4c95edb176f0
Marian2021 <79292105+Marian2021@users.noreply.github.com>
2021-03-03 15:27:53 -0600

    Update photo.php

--- photo.php
+++ photo.php
@@ -1,19 +1,64 @@
+public function setPhotoId($photoId){
+         $this->photoId = $photoId;
+      }
+public function getPhotoId(){
+         return $this->photoId;
+}
+
 <?php
 class photo {
-	public photoId;
-	public userId;
-	public albumId
-	public description;
-	public title;
-	public pictureDirectory;
-	function __construct($photoId, $userId, $albumId, $description, $description, $pictureDirectory) {
-    $this->photoId = $photoId;
-	$this->userId = $userId;
-	$this->albumId = $albumId;
-	$this->description = $description;
-	$this->title = $title;
-	$this->pictureDirectory = $pictureDirectory;
-  }
+   public photoId;
+   public userId;
+   public albumId
+   public description;
+   public title;
+   public pictureDirectory;
+   function __construct($photoId, $userId, $albumId, $description, $description, $pictureDirectory) {
+                        $this->photoId = $photoId;
+                   $this->userId = $userId;
+                   $this->albumId = $albumId;
+                   $this->description = $description;
+                   $this->title = $title;
+                   $this->pictureDirectory = $pictureDirectory;
+        }
+        public function setPhotoId($photoId){
+         $this->photoId = $photoId;
+      }
+        public function getPhotoId(){
+         return $this->photoId;
+        }
+        public function setUserId($userId){
+         $this->userId = $userId;
+      }
+        public function getUserId(){
+         return $this->userId;
+        }
+        public function setAlbumId($albumId){
+         $this->albumId = $albumId;
+      }
+        public function getAlbumId(){
+         return $this->albumId;
+        }
+        public function setDescription($description){
+         $this->description = $description;
+      }
+        public function getDescription(){
+         return $this->description;
+        }
+        public function setTitle($title){
+         $this->title = $title;
+      }
+        public function getTitle(){
+         return $this->title;
+        }
+        public function setPictureDirectory($php){
+         $this->pictureDirectory = $pictureDirectory;
+      }
+        public function getPictureDirectory(){
+         return $this->pictureDirectory;
+        }
+        
+        
 }
 
 
