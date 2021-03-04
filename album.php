daa71eb78ff0768e50bfe4a7d61436a8e76eba42
NoahBmetro <79676018+NoahBmetro@users.noreply.github.com>
2021-03-03 20:26:41 -0600

    Update album.php
    Added getters and setters

--- album.php
+++ album.php
@@ -10,6 +10,30 @@ class album {
 	$this->title = $title;
 	$this->description = $description;
   }
+	public function setAlbumId($albumId){
+		$this->albumId = $albumId;
+	}
+	public function getAlbumId(){
+		return $this->albumId;
+	}
+	public function setUserId($userId){
+		$this->userId = $userId;
+	}
+	public function getUserId(){
+		return $this->userId;
+	}
+	public function setTitle($title){
+		$this->title = $title;
+	}
+	public function getTitle(){
+		return $this->title;
+	}
+	public function setDescription($description){
+		$this->description = $description;
+	}
+	public function getDescription(){
+		return $this->description;
+	}
 }
 
 
