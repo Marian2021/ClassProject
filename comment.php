a04a97b3cf4c6dc0ee343420dd534b7c2c1c273b
NoahBmetro <79676018+NoahBmetro@users.noreply.github.com>
2021-03-03 20:30:39 -0600

    Update comment.php
    Added getters and setters

--- comment.php
+++ comment.php
@@ -8,6 +8,24 @@ class comment {
 	$this->pictureId = $pictureId;
 	$this->commentContent = $commentContent;
   }
+	public function setCommentId($commentId){
+		$this->commentId = $commentId;
+	}
+	public function getCommentId(){
+		return $this->commentId;
+	}
+	public function setPictureId($pictureId){
+		$this->pictureId = $pictureId;
+	}
+	public function getPictureId(){
+		return $this->pictureId;
+	}
+	public function setCommentContent($commentContent){
+		$this->commentContent = $commentContent;
+	}
+	public function getCommentContent(){
+		return $this->commentContent;
+	}
 }
 
 
