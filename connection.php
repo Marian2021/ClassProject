93bfef3e070c7bdadea3196027f7ab3e3248a4c8
vc7313gr <70454874+vc7313gr@users.noreply.github.com>
2021-03-03 21:32:42 -0600

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
-$db = mysqli_connect('localhost', 'root', '', 'photoSite');
+$db = new mysqli_connect('localhost', 'root', '', 'photoSite');
+#can be changed to have variables to allow for connection objects to be created for multiple users
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 ?>
