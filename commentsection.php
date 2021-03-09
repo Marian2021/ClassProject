80bd31f37948c9ea2e3bf829c374b4ff3033e7a7
ronmacrita <63679978+maritaMachogu@users.noreply.github.com>
2021-03-09 00:18:22 -0600

    Update commentsection.php

--- commentsection.php
+++ commentsection.php
@@ -1,31 +1,53 @@
-
-
 <?php
-class commentSection {
-	public sectionId;
-	public pictureId;
-	function __construct($sectionId, $pictureId) {
-    $this->sectionId = $sectionId;
-	$this->pictureId = $pictureId;
+class user {
+ 
+	public  email;
+	public username;
+	private userId;
+	private permissions;
+	private isAdmin;
+	
+	function __construct($email, $username, $userId, $permissions, $isAdmin) {
+    $this->email = $email;
+	$this->username = $username;
+	$this->userId = $userId;
+	$this->permissions = $permissions;
+	$this->isAdmin = $isAdmin;
   }
-
-  //getters
-  public function getsectionId($sectionId){
-    return $this->sectionId= $sectionId;
+	//setters
+public function setemail($email){
+   return $this->email= $email;
 }
-public function getpictureId($pictureId){
-    return $this->pictureId= $pictureId;
+public function setusername($username){
+    return  $this->username= $username;
 }
-
-//setter
-public function setsectionId($sectionId){
-    return $this->sectionId= $sectionId;
+public function setuserId($userId){
+    return $this->userId= $userId;
 }
-public function setpictureId($pictureId){
-    return $this->pictureId= $pictureId;
+public function setpermissions($permissions){
+    return $this->permissions = $permissions;
 }
-
+public function setisAdmin($isAdmin){
+    return $this->isAdmin= $isAdmin;
 }
 
+//getters
+public function getemail($email){
+    return $this->email= $email;
+}
+public function getusername($username){
+    return $this->username= $username;
+}
+public function getuserId($userId){
+    return $this->userId= $userId;
+}
+public function getpermissions($permissions){
+    return $this->permissions = $permissions;
+}
+public function getisAdmin($isAdmin){
+    return $this->isAdmin= $isAdmin;
+}
 
+
+}
 ?>
