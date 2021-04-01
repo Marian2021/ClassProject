1635c853341cb9c2c19793e9724ae3ddb01aa366
vc7313gr <70454874+vc7313gr@users.noreply.github.com>
2021-04-01 18:38:23 -0500

    photo posting
    trying to adapt code from Marian to upload pictures, running into these errors in picturepost.php
    Warning: Undefined array key "image" in C:\xampp\htdocs\photosite\picturepost.php on line 5
    
    Warning: Trying to access array offset on value of type null in C:\xampp\htdocs\photosite\picturepost.php on line 5
    
    Warning: Undefined array key "image" in C:\xampp\htdocs\photosite\picturepost.php on line 11
    
    Warning: Trying to access array offset on value of type null in C:\xampp\htdocs\photosite\picturepost.php on line 11

--- picturepost.php
+++ picturepost.php
@@ -2,20 +2,22 @@
 include_once 'db.php';
 if(isset($_POST['submit']))
 {    
-
+	$target_path = 'images/'.basename($_FILES['image']['name']);
 	$pictureId = mysqli_real_escape_string($conn, $_POST['pictureId']);
 	$userId = mysqli_real_escape_string($conn, $_POST['userId']);
 	$albumId = mysqli_real_escape_string($conn, $_POST['albumId']);
 	$pictureDescription = mysqli_real_escape_string($conn, $_POST['pictureDescription']);
 	$pictureTitle = mysqli_real_escape_string($conn, $_POST['pictureTitle']);
-	$pictureDirectory = mysqli_real_escape_string($conn, $_POST['pictureDirectory']);
+	$image = basename($_FILES['image']['name']);
+	//$pictureDirectory = mysqli_real_escape_string($conn, $_POST['pictureDirectory']);
 
-    $sql = "INSERT INTO picture (pictureId,userId,albumId,pictureDescription,pictureTitle,pictureDirectory) VALUES ('$pictureId','$userId','$albumId','$pictureDescription','$pictureTitle','$pictureDirectory')";
-    if (mysqli_query($conn, $sql)) {
-		echo "New record has been added successfully !";
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($conn);
-     }
-     mysqli_close($conn);
+		$sql = "INSERT INTO picture (pictureId,userId,albumId,pictureDescription,pictureTitle,pictureDirectory) VALUES ('$pictureId','$userId','$albumId','$pictureDescription','$pictureTitle','$image')";
+		if (mysqli_query($conn, $sql)) {
+			echo "New record has been added successfully !";
+		 } else {
+			echo "Error: " . $sql . ":-" . mysqli_error($conn);
+		 }
+		 mysqli_close($conn);
+
 }
 ?>
