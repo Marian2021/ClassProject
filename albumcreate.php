9a6d2274194abedaab3a7c03214a981823b97006
NoahBmetro <79676018+NoahBmetro@users.noreply.github.com>
2021-04-08 15:02:26 -0500

    Add files via upload

---
+++ albumcreate.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Complete Bootstrap 4 Website Layout</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="stlyle.css" rel="stylesheet">
+  <title>ALbum Create</title>
+  <link rel="stylesheet" type="text/css" href="album.css">
+</head>
+<body>
+    <!-- Navigation -->
+<nav class ="navbar navbar-expand-md navbar-light bg-light sticky-top">
+    <div class="container-fluid">
+	<a class="navbar-brand" href="#"><img src="img/logo.png"></a> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarResponsive">
+    <span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse"id ="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="register.php">Register</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
+  <div class="header">
+  	<h2>Create Album</h2>
+  </div>
+	
+  <form method="post" action="albumview.php">
+  	<div class="form-group">
+      <label for="filename">Upload images for album</label>
+      <input type="file" class="form-control-file" id="filename" multiple>
+    </div>
+  </form>
+</body>
+</html>
